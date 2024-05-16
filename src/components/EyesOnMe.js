73c1e2000330bd557e1c05983e5c27360c0d38e8
Jessica Pichardo <jess.pichardo18@gmail.com>
2024-05-15 20:06:33 -0700

    complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+
+function handleFocus(event){
+    console.log('Good!')
+}
+  function handleBlur(event){
+    console.log('Hey! Eyes on me!')
+  }  
+
+return (
+    <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on meq</button>
+)
+
+}
+
+export default EyesOnMe;
